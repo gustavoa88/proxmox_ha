a1a7d01fe13f9fba2b5a2db3e348d73fa1f4f46b
gustavoa88 <gustavoa88@yahoo.com.br>
2024-05-16 23:32:46 -0300

    Update ubunti_ha_v2.sh

--- ubunti_ha_v2.sh
+++ ubunti_ha_v2.sh
@@ -26,3 +26,22 @@ EOF
 header_info
 echo -e "\n Loading..."
 
+get_latest_ubuntu_iso() {
+    # Baixa a página HTML do site do Ubuntu Server
+    curl -s "https://ubuntu.com/download/server" > ubuntu_server.html
+    
+    # Extrai o link do arquivo ISO mais recente
+    latest_iso=$(grep -o 'https://releases.ubuntu.com/[0-9]+\.[0-9]+/ubuntu-[0-9]+\.[0-9]+.[0-9]+-server-amd64\.iso' ubuntu_server.html | head -n1)
+    
+    # Extrai apenas o nome do arquivo ISO do link
+    iso_filename=$(echo "$latest_iso" | awk -F'/' '{print $NF}')
+    
+    # Exibe o nome do arquivo ISO mais recente
+    echo "O arquivo ISO mais recente do Ubuntu Server é: $iso_filename"
+    
+    # Limpa o arquivo HTML temporário
+    rm ubuntu_server.html
+}
+
+# Chama a função
+get_latest_ubuntu_iso
