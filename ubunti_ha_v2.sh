7c066425604f61a2ed99185f3a9e93cb0306b52e
gustavoa88 <gustavoa88@yahoo.com.br>
2024-05-17 00:37:17 -0300

    Update ubunti_ha_v2.sh

--- ubunti_ha_v2.sh
+++ ubunti_ha_v2.sh
@@ -24,13 +24,13 @@ function header_info {
 EOF
 }
 header_info
-echo -e "\n Loading... \n v_005"
+echo -e "\n Loading... \n v_006"
 
 
 # Função para obter a última versão do Ubuntu Server
 get_latest_ubuntu_server_iso() {
     # Baixa a página HTML do site do Ubuntu Server
-    curl -s "https://ubuntu.com/download/server" > ubuntu_server.html
+    curl -s "view-source:https://ubuntu.com/download/server" > ubuntu_server.html
     
     # Exibe o conteúdo do arquivo ubuntu_server.html
     cat ubuntu_server.html
