381476f7089960ef38b70e8f14f5a59eb5afd299
gustavoa88 <gustavoa88@yahoo.com.br>
2024-05-16 23:41:26 -0300

    Update ubunti_ha_v2.sh

--- ubunti_ha_v2.sh
+++ ubunti_ha_v2.sh
@@ -28,9 +28,18 @@ echo -e "\n Loading..."
 
 # Função para obter a última versão do Ubuntu Server
 get_latest_ubuntu_server_iso() {
-    UBUNTU_URL=$(curl -s https://releases.ubuntu.com | grep -oP 'https:\/\/releases.ubuntu.com\/\K[0-9]+(\.[0-9]+)?(?=\/)' | sort -V | tail -n 1)
+    # Baixa a página HTML do site do Ubuntu Server e filtra a versão mais recente
+    UBUNTU_URL=$(curl -s https://ubuntu.com/download/server | grep -oP 'https:\/\/releases.ubuntu.com\/\K[0-9]+(\.[0-9]+)?(?=\/)' | sort -V | tail -n 1)
+    
+    # Constrói a URL do arquivo ISO
     ISO_URL="https://releases.ubuntu.com/$UBUNTU_URL/ubuntu-$UBUNTU_URL-live-server-amd64.iso"
+    
+    # Extrai o nome do arquivo ISO
+    ISO_FILENAME="ubuntu-$UBUNTU_URL-live-server-amd64.iso"
+    
+    # Exibe a última versão do Ubuntu Server e o URL do arquivo ISO
     echo "Última versão do Ubuntu Server: $UBUNTU_URL"
+    echo "Nome do arquivo ISO: $ISO_FILENAME"
     echo "URL do ISO: $ISO_URL"
 }
 
